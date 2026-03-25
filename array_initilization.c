78ec54f30478c49ae967862283343ae6221d2475
sakshihond02 <sakshihond@gmail.com>
2026-03-25 21:18:07 +0530

    Add files via upload

---
+++ array_initilization.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a[3][2]={{1,2}{3,4}{5,6}};
+	int i,j;
+	printf("Display Array Elements :");
+	for(i=0; i<3; i++)
+	{
+		for(j=0; j<2; j++)
+		{
+			printf("%d",a[i][j]);
+		}
+	}
+}
